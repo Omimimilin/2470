6853add0b1562ffba741d2668a75dcf16fd81b36
Naomi <naomilin4668@gmail.com>
2026-09-02 15:31:55 -0500

    finished challanege for 9/2

--- Week2/Hello.java
+++ Week2/Hello.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class Hello {
     public static void main(String[] args) {
 
-        //challenge #1 - printing
+        /*//challenge #1 - printing
         System.out.println("Hello World!");
 
         //challenge #2 - variables
@@ -109,5 +110,99 @@ public class Hello {
             again = "n";
         }
         System.out.println("Thank you for using the calculator.");
+        */
+
+        /*//challenge #7 - calculate 5 test scores
+        Scanner scan = new Scanner(System.in);
+        int index = 0;
+        int count = 5;
+        int input = 0;
+        int total = 0;
+        int average = 0;
+        int highest = Integer.MIN_VALUE;
+        int lowest = Integer.MAX_VALUE;
+        char grade = 'A';
+        int[] arr = new int[5];
+        while(index < 5){
+            System.out.printf("Please input %d more test scores%n", count);
+            input = scan.nextInt();
+            arr[index] = input;
+            total += input;
+            if(input > highest){
+                highest = input;
+            }
+            if(input < lowest){
+                lowest = input;
+            }
+            index++;
+            count--;
+        }
+        average = total/index;
+        System.out.printf("Total: %d%n", total);
+        System.out.printf("Average: %d%n", average);
+        System.out.printf("Highest: %d%n", highest);
+        System.out.printf("Lowest: %d%n%n", lowest);
+        System.out.println("Your values were:");
+        for(int i = 0; i < arr.length; i++){
+            printGrade(arr[i]);
+        }
+        System.out.println(""); */
+
+
+        //challenge #8 Repl
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        double balance = 0;
+        while(input != 4){
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            input = scan.nextInt();
+            if(input == 1){
+                System.out.printf("Balance: $%.2f%n", balance);
+            } else if(input == 2){
+                System.out.println("Please enter an amount");
+                input = scan.nextInt();
+                while(input <= 0){
+                    System.out.println("Please enter an amount more than $0.00");
+                    input = scan.nextInt();
+                }
+                balance += input;
+                System.out.printf("Deposit $%d.00%nBalance: $%.2f%n", input, balance);
+                input = 0;
+            } else if(input == 3){
+                if(balance > 0){
+                    System.out.println("Please enter an amount");
+                    input = scan.nextInt();
+                    while(input <= 0 || balance - input < 0 ){
+                        System.out.println("Please enter a valid amount of more than $0.00 or you do not have withdrawable amount in your account");
+                        input = scan.nextInt();
+                    }
+                    balance -= input;
+                    System.out.printf("Withdraw $%d.00%nBalance: $%.2f%n", input, balance);
+                    input = 0;
+                } else{
+                    System.out.println("You do not have any money to withdraw from");
+                }
+            }
+        }
+        scan.close();
+    }
+
+    public static void printGrade(int score){
+            char grade;
+            if(score >= 90){
+            grade = 'A';
+        } else if(score > 75){
+            grade = 'B';
+        } else if(score > 60){
+            grade = 'C';
+        } else if(score > 20){
+            grade = 'D';
+        } else{
+            grade = 'F';
+        }
+        System.out.printf("%d - %c%n", score, grade);
     }
 }
